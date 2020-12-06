214a6a1f80600d3ee1724a094234f7de8e6398fe
Jichun Quan <jichun.quan@gmail.com>
2020-12-06 22:41:34 +0900

    Change portfolio

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ module.exports = {
     // Site URL for when it goes live
     siteUrl: `https://phantom-assassin-portfolio/quanjichun`,
     // Your Name
-    name: '전기춘(Jichun Quan)',
+    name: '전기춘 / 全基春 / Jichun Quan',
     // Main Site Title
     title: `Jichun Quan | Front-end Developer`,
     // Description that goes under your name in main bio
@@ -36,7 +36,7 @@ module.exports = {
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
-        name: '1. ID 업무 디지털화(DX) 시스템',
+        name: 'ID 업무 디지털화(DX) 시스템',
         duration: '2020.01 ~ 진행 중',
         description: {
           content: [
@@ -63,34 +63,46 @@ module.exports = {
         link: '',
       },
       {
-        name: '3. Hotel Mode App/관련 service 구현 및 유지보수',
+        name: 'Youtube/Amazon PV app porting',
+        duration: '2019.01 ~ 2019.12(Youtube), 2019.11 ~ 2019.12(Amazon)',
+        description: {
+          content: [
+            '가정용 TV에서 동작하는 Youtube/Amazon PV App을 Commercial TV 에서 동작하도록 porting 함',
+          ],
+          contribution: [
+            'Commercial TV platform 환경에 맞는 App build script 구현',
+            'Commercial TV platform 환경에서 발생하는 이슈 해결 및 Commercial 특화기능 추가'
+          ],
+          skills: [
+            'Shell Script, C++'
+          ],
+          achivement: []
+        },
+        link: '',
+      },
+      {
+        name: 'Hotel Mode App/관련 service 구현 및 유지보수',
         duration: '2017.07 ~ 2019.12',
         description: {
           content: [
-            '개발자/팀 이슈관리, 프로젝트 개발관리, 업무보고, 업무 데이터(email, voc, rms 등) 수집/분석/visualization, Task 관리, \
-            매출정보 visualization 등 기능을 포함하고 있는 종합업무 시스템',
+            '가정용 TV의 Hotel Mode 기능 구현 및 유지보수',
           ],
           contribution: [
-            '프로젝트(모델) 개발 관리시스템 구현',
-            '업무보고 시스템 구현',
-            'Elasticsearch 기반 통합검색 구현',
-            'Task 관리 시스템 구현',
+            'Hotel Mode App refactoring',
+            '일부 TV App/Service에 Hotel Mode 제약사항 추가',
+            'Hotel Mode 기능 유지보수'
           ],
           skills: [
-            'Front-end: React, Material-UI, CKEditor, styled-components',
-            'Back-end: GraphQL, Koa, MySql, Sequelize',
-            'Others: Elasticsearch, Python, JTest',
+            'Enact(React based webOS TV UI framework), Redux'
           ],
           achivement: [
-            '현재 ID 개발담당에서 해당 업무시스템을 사용하고 있음',
-            'Agile 개발방식과 web fullstack 개발경험을 얻었음.',
-            'GraphQL, Elasticsearch와 같은 up-to-date 개발기술을 익혔음',
+            '타 사업부 여러팀의 다양한 인원과 함께 개발하면서 Communication 능력과 협업능력이 많이 향상하였음'
           ],
         },
         link: '',
       },
       {
-        name: '3. Group Manager Signage/Hotel App',
+        name: 'Group Manager Signage/Hotel App',
         duration: '2016.11 ~ 2017.03, 2019.02 ~ 2019.03(Refactoring)',
         description: {
           content: [
@@ -103,8 +115,7 @@ module.exports = {
           ],
           skills: [
             'Enyo(webOS TV UI framework)',
-            'Enact(React based webOS TV UI framework)',
-            'Redux'
+            'Enact(React based webOS TV UI framework), Redux'
           ],
           achivement: [
             '2017년 Signage, 부분 Hotel TV부터 최신 모델까지 계속 탑재되어 출시하고 있음',
@@ -113,6 +124,54 @@ module.exports = {
         },
         link: '',
       },
+      {
+        name: 'IME Commercial 기능 개발 및 유지보수',
+        duration: '2015.10 ~ 2017.06, 2019.07 ~ 2019.08',
+        description: {
+          content: [],
+          contribution: [
+            '이형 Signage(3840 * 600, 600 * 3840) 해상도에 맞게 UI 수정',
+            'Commercial TV/Signage 환경에서 발생하는 이슈 해결'
+          ],
+          skills: [
+            'QML, C++'
+          ],
+          achivement: [],
+        },
+        link: '',
+      },
+      {
+        name: 'Signage 특화앱 개발 및 유지보수',
+        duration: '2014.12 ~ 2018.12',
+        description: {
+          content: [],
+          contribution: [
+            'Singage 정보를 보여주는 Information App 신규개발 및 유지보수',
+            'SI App 설치에 사용되는 SI App Setting 신규개발 및 유지보수',
+            'ISM 잔상보상 App 신규개발 및 유지보수'
+          ],
+          skills: [
+            'QML, Enyo'
+          ],
+          achivement: [],
+        },
+        link: '',
+      },
+      {
+        name: 'Commercial TV 채널관련 기능',
+        duration: '2016.01 ~ 2019.12',
+        description: {
+          content: [],
+          contribution: [
+            'Channel Tuning, Channel Manager, Live TV, LiveMenu Commercial 기능 개발 및 유지보수'
+          ],
+          skills: [
+            'QML, Enyo'
+          ],
+          achivement: [],
+        },
+        link: '',
+      }
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
     experience: [
--- src/components/section-about/index.jsx
+++ src/components/section-about/index.jsx
@@ -6,7 +6,7 @@ const classes = {
   list: 'mb-6',
   item: 'inline list-none pr-4',
   link:
-    'inline-block py-2 font-semibold text-sm text-gray-600 hover:text-black'
+    'inline-block py-2 font-semibold text-base text-gray-600 hover:text-black'
 };
 
 const SectionAbout = ({ about }) => {
